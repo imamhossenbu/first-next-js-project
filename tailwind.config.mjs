e543254001a986e8e65cdf8b0f1ae8009fb0a31b
imamhossenbu <163906693+imamhossenbu@users.noreply.github.com>
2025-01-24 01:53:49 +0600

    modify the code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
